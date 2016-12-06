71e438e132689d2e05bd6bd1a3559952dd161dea
Eddie S <eddie@visualead.com>
2016-12-06 13:55:14 +0200

    improve doc

--- elo_test.py
+++ elo_test.py
@@ -1,9 +1,13 @@
 """
 Usage:
 - command line:
-python3 elo.py winner_rank loser_rank
+python3 elo_test.py winner_rank loser_rank
 - output:
 new_winner_rank new_loser_rank
+
+Example:
+python3 elo_test.py 1200 1200
+1216 1184
 """
 import math
 import sys
